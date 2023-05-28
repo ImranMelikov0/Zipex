226342d6c21c5c7ffa9ea64eec52f713ccc0a133
Imran Melikov <imran.melikov70@gmail.com>
2023-05-28 18:41:38 +0400

    fragments added

---
+++ app/src/main/java/com/imranmelikov/zipex/view/AdminDetailFragment.kt
@@ -0,0 +1,19 @@
+package com.imranmelikov.zipex.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.imranmelikov.zipex.R
+
+class AdminDetailFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_admin_detail, container, false)
+    }
+}
