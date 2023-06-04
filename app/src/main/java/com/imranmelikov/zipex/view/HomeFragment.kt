31d8ce8a9cbc6c3b214a2daaaec99c6954a0aa13
Imran Melikov <imran.melikov70@gmail.com>
2023-06-05 03:51:31 +0400

    design ended without drawer

--- app/src/main/java/com/imranmelikov/zipex/view/HomeFragment.kt
+++ app/src/main/java/com/imranmelikov/zipex/view/HomeFragment.kt
@@ -1,20 +1,39 @@
 package com.imranmelikov.zipex.view
 
+import android.graphics.Rect
 import android.os.Bundle
+import android.view.Gravity
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.internal.NavigationMenuView
+import com.google.android.material.navigation.NavigationView
 import com.imranmelikov.zipex.R
+import com.imranmelikov.zipex.databinding.FragmentHomeBinding
 
 // T
 class HomeFragment : Fragment() {
+    private lateinit var binding:FragmentHomeBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        binding= FragmentHomeBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+
     }
 }
