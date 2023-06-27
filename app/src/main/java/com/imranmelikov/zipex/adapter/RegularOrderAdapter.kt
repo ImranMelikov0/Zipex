32965e1a75ce7783f343c49cb77866bb33f9a5e7
Imran Melikov <imran.melikov70@gmail.com>
2023-06-27 05:45:51 +0400

    progressBar added into imageView

--- app/src/main/java/com/imranmelikov/zipex/adapter/RegularOrderAdapter.kt
+++ app/src/main/java/com/imranmelikov/zipex/adapter/RegularOrderAdapter.kt
@@ -1,14 +1,22 @@
 package com.imranmelikov.zipex.adapter
 
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
 import android.net.Uri
+import android.transition.Transition
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.AsyncListDiffer
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.RequestManager
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.CustomTarget
 import com.imranmelikov.zipex.databinding.RegularOrderRowBinding
 import com.imranmelikov.zipex.model.Notification
 import com.imranmelikov.zipex.model.RegularOrder
@@ -47,7 +55,35 @@ class RegularOrderAdapter @Inject constructor(
 
     override fun onBindViewHolder(holder: RegularOrderViewHolder, position: Int) {
         val regularOrderArraylist=regularOrderList.get(position)
+
+        holder.binding.progressBar.visibility=View.VISIBLE
+
+
         glide.load(regularOrderArraylist.imgUrl)
+            .listener(object : RequestListener<Drawable> {
+                override fun onLoadFailed(
+                    e: GlideException?,
+                    model: Any?,
+                    target: com.bumptech.glide.request.target.Target<Drawable>?,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    // Yükleme başarısız olduğunda ProgressBar'ı gizleyin
+                    holder.binding.progressBar.visibility = View.GONE
+                    return false
+                }
+
+                override fun onResourceReady(
+                    resource: Drawable?,
+                    model: Any?,
+                    target: com.bumptech.glide.request.target.Target<Drawable>?,
+                    dataSource: DataSource?,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    // Yükleme tamamlandığında ProgressBar'ı gizleyin
+                    holder.binding.progressBar.visibility = View.GONE
+                    return false
+                }
+            })
             .into(holder.binding.regularImage)
 
         holder.itemView.setOnClickListener {
--- app/src/main/java/com/imranmelikov/zipex/di/AppModule.kt
+++ app/src/main/java/com/imranmelikov/zipex/di/AppModule.kt
@@ -3,12 +3,14 @@ package com.imranmelikov.zipex.di
 import android.content.Context
 import android.content.SharedPreferences
 import android.graphics.drawable.Drawable
+import android.view.LayoutInflater
 import android.widget.ProgressBar
 import androidx.preference.PreferenceManager
 import androidx.room.Room
 import com.bumptech.glide.Glide
 import com.bumptech.glide.RequestManager
 import com.bumptech.glide.request.RequestOptions
+import com.imranmelikov.zipex.R
 import com.imranmelikov.zipex.db.ProjectZipexDao
 import com.imranmelikov.zipex.db.ProjectZipexDatabase
 import com.imranmelikov.zipex.repo.ZipexRepo
@@ -40,12 +42,13 @@ fun injectDatabase(@ApplicationContext context:Context)=Room.databaseBuilder(
 
     @Singleton
     @Provides
-    fun injectGlide(@ApplicationContext context: Context):RequestManager {
-        val progressBar = ProgressBar(context)
+    fun injectGlide(@ApplicationContext context: Context): RequestManager {
+        val progressBar = LayoutInflater.from(context).inflate(R.layout.progressbar, null) as ProgressBar
         val progressBarDrawable: Drawable = progressBar.indeterminateDrawable
         val requestOptions = RequestOptions()
             .placeholder(progressBarDrawable)
             .error(progressBarDrawable)
-      return  Glide.with(context).setDefaultRequestOptions(requestOptions)
+        return Glide.with(context).setDefaultRequestOptions(requestOptions)
     }
-    }
+
+}
